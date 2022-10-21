030354411b6350d480b3af789b2882a00ca0b1d9
danila <danila_glazkov@mail.ru>
2022-10-21 15:59:54 +0400

    change byte[] to BitArray

--- Watermarking/Program.cs
+++ Watermarking/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.Sqlite;
 using NetTopologySuite.IO.VectorTiles.Mapbox;
 using System;
+using System.Collections;
 using System.IO;
 
 namespace Watermarking
@@ -11,9 +12,10 @@ namespace Watermarking
         {
             //var path = "C:\\Users\\user\\source\\Watermarking\\828.mvt";
 
-            //var path = "C:\\Users\\user\\source\\Watermarking\\stp.mbtiles";
-            var path = "C:\\Users\\user\\source\\Watermarking\\polygons.mvt";
-            var path2 = "C:\\Users\\user\\source\\Watermarking\\polygons_watermarked.mvt";
+            var path = "C:\\Users\\user\\source\\Watermarking\\stp.mbtiles";
+            //var path = "C:\\Users\\user\\source\\Watermarking\\isogd4.mvt";
+            var path2 = "C:\\Users\\user\\source\\Watermarking\\stp656_334_10.mvt";
+            var path3 = "C:\\Users\\user\\source\\Watermarking\\stp656_334_10_orig.mvt";
 
             //var z = 11;
             //var x = 359;
@@ -39,9 +41,9 @@ namespace Watermarking
             //var y = 333;
 
             //iosgd4
-            //var z = 10;
-            //var x = 656;
-            //var y = 334;
+            var z = 10;
+            var x = 656;
+            var y = 334;
 
             //iosgd5
             //var z = 10;
@@ -53,28 +55,33 @@ namespace Watermarking
             //var x = 2619;
             //var y = 1333;
 
-            var z = 3;
-            var x = 3;
-            var y = 4;
+            //var z = 3;
+            //var x = 4;
+            //var y = 3;
 
-            //using var sqliteConnection = new SqliteConnection($"Data Source = {path}");
-            //sqliteConnection.Open();
+            using var sqliteConnection = new SqliteConnection($"Data Source = {path}");
+            sqliteConnection.Open();
 
-            //using var command = new SqliteCommand(@"SELECT tile_data FROM tiles WHERE zoom_level = $z AND tile_column = $x AND tile_row = $y", sqliteConnection);
-            //command.Parameters.AddWithValue("$z", z);
-            //command.Parameters.AddWithValue("$x", x);
-            //command.Parameters.AddWithValue("$y", (1 << z) - y - 1);
-            //var tile = (byte[])command.ExecuteScalar();
+            using var command = new SqliteCommand(@"SELECT tile_data FROM tiles WHERE zoom_level = $z AND tile_column = $x AND tile_row = $y", sqliteConnection);
+            command.Parameters.AddWithValue("$z", z);
+            command.Parameters.AddWithValue("$x", x);
+            command.Parameters.AddWithValue("$y", (1 << z) - y - 1);
+            var tile = (byte[])command.ExecuteScalar();
 
-            int extent = 4096;
+            uint extent = 4096;
 
             int size = 8;
 
             int key = 0;
 
-            int countpoints = 1;
+            int countpoints = 15;
 
-            var watermark = new Watermark(path, x, y, z, key, size, countPoints: countpoints, extent: extent);
+            int distance = 2;
+
+            var watermark = new Watermark(tile, x, y, z, key, size, countPoints: countpoints, extent: Convert.ToInt32(extent), distance: distance, compressed: true);
+
+            using var fs3 = new FileStream(path3, FileMode.Create);
+            watermark.Tile.Write(fs3, extent);
 
             var count = 0;
             foreach (var layer in watermark.Tile.Layers)
@@ -85,12 +92,14 @@ namespace Watermarking
             Console.WriteLine(count);
             Console.WriteLine(watermark.CountOutside());
 
-            var bytes = new byte[] { 0x33 };
+            //var bytes = new byte[] { 0xFF };
+
+            var bites = new BitArray(new bool[] { true, true, true, true, true, true, true, true });
 
             var t2 = 0.4;
             var delta2 = 0.35;
 
-            watermark.Embed(bytes, t2, delta2);
+            watermark.Embed(bites, t2, delta2);
 
             var tileWatermarked = watermark.Tile;
 
@@ -98,7 +107,7 @@ namespace Watermarking
             //tileWatermarked.Layers.RemoveAt(1);
             //tileWatermarked.Layers.RemoveAt(0);
 
-            var w = new Watermark(tileWatermarked, x, y, z, key, size, countPoints: countpoints, extent: extent, m: watermark.M);
+            var w = new Watermark(tileWatermarked, x, y, z, key, size, countPoints: countpoints, extent: Convert.ToInt32(extent), m: watermark.M, distance: distance);
 
             count = 0;
             foreach (var layer in w.Tile.Layers)
@@ -109,15 +118,37 @@ namespace Watermarking
             Console.WriteLine(count);
             Console.WriteLine(watermark.CountOutside());
 
-            var resBytes = w.GetWatermark(t2);
+            var resBites = w.GetWatermark(t2);
 
-            for (var i = 0; i < size / 8; i++)
-                Console.WriteLine($"{resBytes[i]:x}");
+            for (var i = 0; i < size; i++)
+                Console.WriteLine($"{resBites[i]:x}");
+
+            //for (var j = 0; j<tileWatermarked.Layers.Count; j++)
+            //{
+            //    var layer = tileWatermarked.Layers[j];
+            //    for (var i = 0; i < layer.Features.Count; i++)
+            //    {
+            //        var feature = layer.Features[i];
+            //        Console.WriteLine($"\t{feature.Geometry.IsValid}");
+            //        if (!feature.Geometry.IsValid)
+            //        {
+            //            layer.Features.RemoveAt(i);
+            //            i--;
+            //        }
+            //    }
+            //}
+
+            //foreach (var layer in tileWatermarked.Layers)
+            //    foreach (var feature in layer.Features)
+            //    {
+            //        if (!feature.Geometry.IsValid)
+            //            Console.WriteLine("not valid :c");
+            //    }
 
             using (var fileStream = new FileStream(path2, FileMode.Create))
-                tileWatermarked.Write(fileStream, extent: (uint)extent);
+                tileWatermarked.Write(fileStream, extent: extent);
 
-            var ww = new Watermark(path2, x, y, z, key, size, countPoints: countpoints, extent: extent, m: watermark.M);
+            var ww = new Watermark(path2, x, y, z, key, size, countPoints: countpoints, extent: Convert.ToInt32(extent), m: watermark.M, distance: distance);
 
             count = 0;
             foreach (var layer in ww.Tile.Layers)
@@ -129,7 +160,7 @@ namespace Watermarking
             Console.WriteLine(watermark.CountOutside());
 
             var res = ww.GetWatermark(t2);
-            for (var i = 0; i < size / 8; i++)
+            for (var i = 0; i < size; i++)
                 Console.WriteLine($"{res[i]:x}");
         }
     }
